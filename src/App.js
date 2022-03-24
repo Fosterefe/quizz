8f31fca3e00fbb5f6595505df38723a509b1df8b
alex <gersonperezgerder@gmail.com>
2022-03-23 21:43:07 -0600

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,95 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
+import {questionData} from './Question'
 
 function App() {
+
+  const [currentQuestion, setCurrentQuestion] = useState(0) 
+  const [correct, setCorrect] = useState(0)
+  const [ans, setAns] = useState(0)
+  const [show, setShow] = useState(false)
+  const [chances, setChances] = useState(1)
+  const [qRes, setQRest] = useState([])
+
+  const nextQuestion = (boolean, {option, isCorrect}) => {
+    setCurrentQuestion(currentQuestion === questionData.length - 1 ? questionData.length - 1 : currentQuestion + 1)
+    setAns(ans === 4 ? 4 : ans + 1)
+    setCorrect(() => {
+      if(ans === 4) return correct
+      return boolean === true ? correct + 1 : correct})
+    setQRest(() => {
+      if(ans === 4) return qRes
+      return [...qRes, {option, isCorrect}]
+    })
+  }
+
+  const seeResults = () => {
+    setShow(show ? false : true)
+  }
+
+  const resetQuizz = () => {
+    setChances(chances - 1)
+    if(chances <= 0) return alert('You do not have more chances')
+    setCurrentQuestion(0)
+    setCorrect(0)
+    setAns(0)
+    setShow(false)
+    setQRest([])
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<div className="App">
+    <div className='question-container'>
+        <div className='question-counter'>
+            <h1>
+                Question {currentQuestion + 1}/{questionData.length}
+            </h1>
+        </div>
+        <div className='question'>
+            <h3>{questionData[currentQuestion].question}</h3>
+            {questionData[currentQuestion].answerOption.map((q, index) => (
+            <button className='option' onClick={() => nextQuestion(q.isCorrect, q)} key={index}>{q.option}</button>
+            ))}
+
+        </div>
+        {ans === 4 && (
+        <div className='btns'>
+          <button className='results' onClick={seeResults}>
+            Show results
+          </button>
+          <button onClick={resetQuizz}>
+          Try one more time
+          </button>
+        </div>
+        )}
+        {show && (
+        <h4>Total of correct answers: {correct}</h4>
+        )}
+        {show && (
+              <div>
+              <table style={{ borderCollapse: 'collapse', width: '50%', cursor: 'default' }}>
+                  <thead>
+                    <tr>
+                    <th style={{ border: '1px solid black', width: '50%' }} >Question</th>
+                    <th style={{ border: '1px solid black', width: '25%' }}>Option</th>
+                    <th style={{ border: '1px solid black', width: '25%' }}>Result</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                  {qRes.map((r, index) => (
+                    <tr key={index}>
+                      <td style={{ border: '1px solid black' }}>{questionData[index].question}</td>
+                      <td style={{ border: '1px solid black' }}>{r.option}</td>
+                      <td style={{ border: '1px solid black' }}>{r.isCorrect ? 'Correct' : 'Incorrect'}</td>
+                    </tr>
+                  ))}
+                  </tbody>
+              </table>
+            </div>
+        )}
     </div>
+</div>
   );
 }
 
---
+++ src/Question.js
@@ -0,0 +1,34 @@
+export const questionData = [
+    {
+        question: 'Who is the CEO of Tesla?',
+        answerOption: [
+            {option: 'Bill Gates', isCorrect: false},
+            {option: 'Mark Zukenberg', isCorrect: false},
+            {option: 'Elon Musk', isCorrect: true}
+        ]
+    },
+    {
+        question: 'Where is located the Eiffel Tower?',
+        answerOption: [
+            {option: 'Germany', isCorrect: false},
+            {option: 'France', isCorrect: true},
+            {option: 'United State', isCorrect: false}
+        ]
+    },
+    {
+        question: 'What is the capital of El Salvador?',
+        answerOption: [
+            {option: 'La Libertad', isCorrect: false},
+            {option: 'San Miguel', isCorrect: false},
+            {option: 'San Salvador', isCorrect: true}
+        ]
+    },
+    {
+        question: 'The iPhone was created by which company?',
+        answerOption: [
+            {option: 'Apple', isCorrect: true},
+            {option: 'Intel', isCorrect: false},
+            {option: 'Amazon', isCorrect: false}
+        ]
+    }
+]
